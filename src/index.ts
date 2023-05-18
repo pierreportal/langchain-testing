2bc9aa816634ed528538fa2da90fd946086b25f8
Pierre Portal <pierreportal.mac@gmail.com>
2023-05-18 20:02:24 +0200

    first init

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
-import * as dotenv from "dotenv";
-import { OpenAI } from "langchain";
+import { OpenAI } from "langchain/llms/openai";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import * as fs from "fs";
 
-dotenv.config();
-
-const model = new OpenAI({
-  modelName: "gpt-3.5-turbo",
-  openAIApiKey: process.env.OPENAI_API_KEY,
-});
-
-const res = await model.call(
-  "What's a good idea for an application to build with GPT-3?"
+/* Initialize the LLM to use to answer the question */
+const model = new OpenAI({});
+/* Load in the file we want to do question answering over */
+const text = fs.readFileSync("./src/documents/test.txt", "utf8");
+/* Split the text into chunks */
+const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+const docs = await textSplitter.createDocuments([text]);
+/* Create the vectorstore */
+const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+/* Create the chain */
+const chain = ConversationalRetrievalQAChain.fromLLM(
+  model,
+  vectorStore.asRetriever()
 );
-
+/* Ask it a question */
+const question = "What is a looner ?";
+const res = await chain.call({ question, chat_history: [] });
 console.log(res);
+/* Ask it a follow up question */
+const chatHistory = question + res.text;
+const followUpRes = await chain.call({
+  question: "Was that nice?",
+  chat_history: chatHistory,
+});
+console.log(followUpRes);
